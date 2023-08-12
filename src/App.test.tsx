34e99a9fede44fd87f1445058d9715af48b367c4
Juntao Qiu <juntao.qiu@gmail.com>
2023-08-12 19:17:23 +1000

    restructured the app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders application heading', () => {
-  render(<App/>);
-  const heading = screen.getByText('The Code Oven');
-  expect(heading).toBeInTheDocument();
-});
+describe('Code Oven Application', () => {
+  it('renders application heading', () => {
+    render(<App/>);
+    const heading = screen.getByText('The Code Oven');
+    expect(heading).toBeInTheDocument();
+  });
+})
