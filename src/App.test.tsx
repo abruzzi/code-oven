311f9f2c128938585f209aa3534739d2e4fe6a25
Juntao Qiu <juntao.qiu@gmail.com>
2023-08-12 18:59:53 +1000

    init the repo with the basic setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders application heading', () => {
+  render(<App/>);
+  const heading = screen.getByText('The Code Oven');
+  expect(heading).toBeInTheDocument();
 });
